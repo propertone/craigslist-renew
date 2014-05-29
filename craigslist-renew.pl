9a8cf497df275e24132f3fd3c623c7cd23a56775
Vitaly Shupak <vitaly.shupak@gmail.com>
2014-05-29 01:00:17 -0400

    initial commit - craigslist renewal script

---
+++ craigslist-renew.pl
@@ -0,0 +1,159 @@
+#!/usr/bin/env perl
+# This script logs in to your craigslist account and renews all posts that can be renewed
+# The script should be invoked with a config file in yaml format containing the following info:
+# ---
+# email: <craigslist login>
+# password: <craigslist password>
+# notify: <comma separated list of emails>
+
+use strict;
+use warnings;
+use WWW::Mechanize;
+use Data::Dumper;
+use MIME::Lite;
+use HTML::TreeBuilder;
+use HTML::TableExtract;
+use Getopt::Long;
+use YAML qw( LoadFile );
+use List::MoreUtils qw( zip );
+
+my $mech = WWW::Mechanize->new();
+my $renewed = 0;
+my $check_expired;
+GetOptions('expired' => \$check_expired);
+
+defined($ARGV[0]) || die("Usage: $0 [--expired] <config.yml>\n");
+
+my $config = LoadFile($ARGV[0]);
+my $NOTIFY = $config->{notify};
+my $SUBJECT = "Craigslist Automatic Renewal";
+my $url = "https://accounts.craigslist.org/login";
+
+$mech->get($url);
+
+# log in with username and password
+$mech->submit_form(
+        fields      => {
+            inputEmailHandle    => $config->{email},
+            inputPassword       => $config->{password},
+        }
+    );
+
+# once in a while, Craigslist verifies the login via Captcha
+$mech->quiet(1);
+if ($mech->form_with_fields('inputEmailHandle', 'inputPassword')) {
+    notify("Login failed - requires captcha");
+    exit(1);
+}
+$mech->quiet(0);
+
+# if --expired flag was specified, check for expired posts
+if ($check_expired) {
+    $SUBJECT = "Craigslist post expired";
+    check_expired($mech->content);
+    exit(0);
+}
+
+# look for all listings with a renew button
+my $n=1;
+my @forms;
+foreach my $form ($mech->forms) {
+    if ($form->method() eq "POST") {
+        my $input = $form->find_input('go');
+        if ($input && $input->value() eq "renew") {
+            push @forms, $n;
+        }
+    }
+    $n++;
+}
+
+foreach my $form_id (@forms) {
+    # click the renew button
+    my $form = $mech->form_number($form_id);
+    $mech->submit_form();
+    if ($mech->content() =~  /Your posting can be seen at/) {
+        # fetch the title and link of the confirmation page
+        my $title=""; my $link="";
+        my $root = HTML::TreeBuilder->new_from_content($mech->content());
+        my @t = $root->look_down('_tag' => 'h2', 'class' => 'postingtitle');
+        my @l = $root->look_down('_tag' => 'div', 'class' => 'managestatus')->look_down('_tag' => 'a', 'target' => '_blank');
+        if (@t) {
+            $title = $t[0]->as_trimmed_text();
+        }
+        if (@l) {
+            $link = $l[0]->attr('href');
+        }
+
+        notify("Renewed \"$title\" (" . $link . ")");
+        $renewed++;
+    }
+    else {
+        notify("Could not renew post - " . $form->action );
+    }
+    $mech->back();
+
+    # only renew the first one
+    # comment out the next line to renew all posts
+    last;
+}
+
+# print message in interactive mode or send email when run from cron
+sub notify {
+    my $message = shift @_;
+    if (-t STDIN) {
+        print "$message\n";
+    }
+    else {
+        my $msg = MIME::Lite->new(
+                    To      => $NOTIFY,
+                    Subject => $SUBJECT,
+                    Data    => $message,
+                    );
+        $msg->send;
+    }
+}
+
+# the config file can list postings that should currently be active. This is specified in the config as:
+# required:
+#   - title: My Posting
+#     area: nyc
+#   - title: Another posting
+#     area: nyc
+sub check_expired {
+    my ($content) = @_;
+    my @columns = qw( status manage title area date id );
+    my $te = HTML::TableExtract->new(headers=>\@columns);
+    $te->parse($content);
+    my @tables = $te->tables;
+    my $ts = $tables[0];
+
+    my $required = $config->{'postings'};
+    foreach my $row ($ts->rows) {
+        map {s/^\s*//gm;s/\s*$//gm;s/\n//g;} @$row;
+        my %info = zip @columns, @$row;
+        if ($info{'status'} =~ /active/i) {
+            # check if this posting matches one of the required postings
+            # if so, mark it as active in the required hash
+            foreach my $posting (@$required) {
+                my $title = $posting->{'title'};
+                my $area = $posting->{'area'} // "";
+                if ($info{'title'} =~ /$title/i and $info{'area'} =~ /$area/i) {
+                    $posting->{'active'} = 1;
+                }
+            }
+        }
+        else {
+            last;
+        }
+    }
+    my @expired = ();
+    foreach my $posting (@$required) {
+        if (!defined($posting->{'active'})) {
+            push @expired, $posting->{'title'} . ' (' . $posting->{'area'} . ')';
+        }
+    }
+    if (@expired) {
+        my $msg = "The following posts have expired:\n\n" . join("\n", @expired);
+        notify($msg);
+    }
+}
